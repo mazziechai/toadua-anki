0b023f2962a8ddbea1d0d7de0b85c50db0678c62
Mazzie <mazziechai@gmail.com>
2023-06-01 01:56:01 -0700

    Basic toadua fetching

--- toaduanki/toadua.py
+++ toaduanki/toadua.py
@@ -1,2 +1,23 @@
-def get_toadua_entry(id: str):
-    pass
+import requests
+
+def get_toadua_entry(id: str) -> dict[str, str]:
+    print(id)
+    query = { "action": "search", "query": ["id", id]}
+
+    request = requests.post("https://toadua.uakci.pl/api", json=query)
+
+    response = request.json()
+    print(response)
+    try:
+        result = response["results"][0]
+    except IndexError:
+        print("no results")
+        raise IndexError
+
+    try:
+        return {"id": id, "head": result["head"], "body": result["body"]}
+
+    except KeyError:
+        print("invalid word")
+        raise KeyError
+
--- toaduanki/ui.py
+++ toaduanki/ui.py
@@ -1,8 +1,30 @@
 from aqt.qt import QDialog
 
 from .entry_ui import Ui_Entry
+from .toadua import get_toadua_entry
 
 class EntryDialog(QDialog, Ui_Entry):
     def __init__(self, *args, **kwargs):
         super(EntryDialog, self).__init__(*args, **kwargs)
         self.setupUi(self)
+
+    def accept(self):
+        text = self.plainTextEdit.toPlainText()
+        print(f"Input: {text}")
+
+        entries = text.splitlines()
+
+        ids: list[str] = []
+        _words: list[str] = []
+
+        for entry in entries:
+            if entry.startswith("#"):
+                ids.append(entry.removeprefix("#"))
+            else:
+                _words.append(entry)
+
+        # TODO: make this work with words instead of just ids
+
+        print([get_toadua_entry(id) for id in ids])
+
+        self.close()
