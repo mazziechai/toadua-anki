568a7d2ef4583c2dcf7e5e3f49ff56fa890f3168
Mazzie <mazziechai@gmail.com>
2023-06-02 15:02:50 -0700

    why is this here

--- toaduanki/__init__.py
+++ toaduanki/__init__.py
@@ -1,19 +1,12 @@
-from anki.collection import Collection
 from aqt import mw
 from aqt.qt import QAction, qconnect
 
-from .toadua import add_notes_to_col
 from .ui import EntryDialog
 
 
 def toadua_cards_tool():
-    col: Collection = mw.col  # type: ignore
-
     mw.entry_dialog = entry_dialog = EntryDialog()  # type: ignore
-
-    if entry_dialog.exec():
-        words = entry_dialog.words
-        add_notes_to_col(col, words)
+    entry_dialog.show()
 
 
 action = QAction("Toadua Cards", mw)
